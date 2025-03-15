66429119271f88d3d34eaada55d314fd468ee6c7
Jacob Kuczynski <jkucz@berkeley.edu>
2025-03-14 18:21:01 -0700

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,28 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/education">Education</Link></li>
-                <li><Link to="/experience">Experience</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/about">About Me</Link></li>
-            </ul>
+        <nav className="bg-white shadow-md sticky top-0 z-50">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-center items-center h-16">
+                    <div className="flex space-x-8">
+                        <Link to="/" className="text-gray-600 hover:text-blue-500 transition">
+                            Home
+                        </Link>
+                        <Link to="/education" className="text-gray-600 hover:text-blue-500 transition">
+                            Education
+                        </Link>
+                        <Link to="/experience" className="text-gray-600 hover:text-blue-500 transition">
+                            Experience
+                        </Link>
+                        <Link to="/projects" className="text-gray-600 hover:text-blue-500 transition">
+                            Projects
+                        </Link>
+                        <Link to="/about" className="text-gray-600 hover:text-blue-500 transition">
+                            About Me
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
